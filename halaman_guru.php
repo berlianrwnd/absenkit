08c69de4352ef4e647bc1bafe714c998b9eecb09
09 Berliana Nurwandani <70336392+berlianrwnd@users.noreply.github.com>
2020-08-27 23:41:56 +0700

    Add files via upload

---
+++ halaman_guru.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title> Halaman Guru </title>
+</head>
+<body>
+	<?php 
+	session_start();
+ 
+	if($_SESSION['level']==""){
+		header("location:index.php?pesan=gagal");
+	}
+ 
+	?>
+	<h1> Halaman Guru </h1>
+ 
+	<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b><?php echo $_SESSION['level']; ?></b>.</p>
+	<a href="logout.php"> LOGOUT </a>
+ 
+	<br/>
+	<br/>
+ 
+</body>
+</html>
